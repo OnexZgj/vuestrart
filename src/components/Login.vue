734154099b2658a60766ea4b5443a8e11ba32c45
OnexZgj <474835552@qq.com>
2021-07-22 21:09:49 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login_container">i am login
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
+    点我打开
+  </el-button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  }
+};
+</script>
