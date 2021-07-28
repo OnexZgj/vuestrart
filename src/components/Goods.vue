5d6e514ebb8247f2c2f91a0cee285c8f8388824e
zhanggengjie <zhanggengjie@kingsoft.com>
2021-07-28 22:02:51 +0800

    添加一些列表数据

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -1,10 +1,73 @@
 <template>
-  <div>goods</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+
+    <el-card>
+      <el-table
+          :data="goodsList"
+          stripe
+          style="width: 100%">
+
+        <el-table-column
+            type="index"
+            label="#">
+        </el-table-column>
+
+        <el-table-column
+            prop="goods_name"
+            label="日期"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="goods_price"
+            label="姓名"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="goods_number"
+            label="地址">
+        </el-table-column>
+        <el-table-column
+            prop="goods_number"
+            label="地址">
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Goods"
+  data() {
+    return {
+      goodsList: [],
+      queryInfo:{
+        query:'',
+        pagenum: 1,
+        pagesize:10
+      }
+    }
+  },
+  methods: {
+    async getGoodList() {
+      const {data: res} = await this.$http.get("goods",{params:this.queryInfo})
+      if (res.meta.status != 200) {
+        return this.$message.error("请求商品列表失败")
+      }
+      this.goodsList = res.data
+    }
+  },
+  created() {
+    this.getGoodList()
+  }
 }
 </script>
 
--- src/components/Roles.vue
+++ src/components/Roles.vue
@@ -1,10 +1,56 @@
 <template>
-<div>Roles</div>
+  <div>
+    <!--    头部区域-->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--    角色列表区域-->
+    <el-card>
+
+      <el-button type="primary">添加角色</el-button>
+      <el-table :data="rolelist" stripe border>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column prop="roleName" label="角色名称"></el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
+        <el-table-column label="操作">
+
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+          </template>
+
+        </el-table-column>
+      </el-table>
+
+    </el-card>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Welcome"
+  data() {
+    return {
+      rolelist: []
+    }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList() {
+      const {data: res} = await this.$http.get('roles')
+      console.log(res)
+      if (res.meta.status != 200) {
+       return  this.$message.error("获取角色列表失败")
+      }
+      this.rolelist = res.data;
+    }
+  }
 }
 </script>
 
