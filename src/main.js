91ebc1251d194727c2858076f3d4b3f6a8a05839
zhanggengjie <zhanggengjie@kingsoft.com>
2021-08-02 19:59:16 +0800

    添加nprogress优化显示

--- src/main.js
+++ src/main.js
@@ -17,13 +17,24 @@ import echarts from 'echarts'
 
 Vue.prototype.$echarts = echarts
 
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 //配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config=>{
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config;
+})
+
 
 Vue.prototype.$http = axios
 
